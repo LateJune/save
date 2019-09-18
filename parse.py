7b85209d8a06d6c8c63b1907f98ff0b536c96ef1
Jonathan <johnsoler24@gmail.com>
2019-09-18 17:57:33 -0400

    updated save to output to .md

--- parse.py
+++ parse.py
@@ -1,62 +1,42 @@
 #!/usr/bin/env python3
 
+# Know when a comment is made
+
 import os
 import time
 
 def main():
-    dir_dict = walk_dir()
-    for entry in dir_dict:
-        print(entry)
-        print(dir_dict[entry])
-        break
-
-    open_file = open("notes.txt", 'r')
-    file_contents = open_file.read()
     
+    open_file = open("notes.md", 'r')
+    file_contents = open_file.readlines()
 
-    parse_single_save_file(file_contents)
+    parse_notes(file_contents)
+
+    print(file_contents)
     return
 
-def parse_single_save_file(file_contents):
-    entry_list = []
-    buffer = []
-    clean_list = []
-    
-    for ch in file_contents:
-        if ch == '`':
-            entry_list.append(''.join(buffer))
-            buffer = []
-        else:
-            buffer.append(ch)
+def parse_notes(file_contents):
+    # two dictionaries????
 
-    for entry in entry_list:
-        if not entry == '' and not entry == '\n':
-            clean_list.append(entry)
-        print(clean_list)
+    date_list = []
+    comment_list = []
+    command_list = []
+    output_list = []
 
-    return clean_list
+    for entry in file_contents:
+        if '####' in entry:
+            date_list.append(entry)
+        elif '- ' in entry:
+            # if there is no comment
+            comment_list.append(entry)
+        elif '```' in entry:
+            command_list.append(entry)
+        elif '`' in entry:
+            output_list.append(entry)
 
-def walk_dir():
-    cwd = os.getcwd()
-    file_path_dic = {}
+    return 
 
-    subdir_list = []
-    file_list = []
-    
-    for subdir, dir, files in os.walk(cwd):
-        subdir_list.append(subdir)
-        for file in files:
-            file_path = os.path.join(subdir,file)
-            #print(file_path)
-            open_file = open(file_path,'r')
-            file_list.append(file)
 
-            if subdir not in file_path_dic:
-                file_path_dic[subdir] = []
-            
-            file_path_dic[subdir].append(file)
-
-    return file_path_dic
 
 if __name__ == "__main__":
     main()
