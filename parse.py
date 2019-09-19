b2c709c4c442918a337013bef761ff8cf69fde73
Jonathan <johnsoler24@gmail.com>
2019-09-18 21:32:26 -0400

    Updated save, fixed parse output

--- parse.py
+++ parse.py
@@ -8,34 +8,67 @@ import time
 def main():
     
     open_file = open("notes.md", 'r')
+    
     file_contents = open_file.readlines()
 
-    parse_notes(file_contents)
-
-    print(file_contents)
+    parse_notes(file_contents)    
+    open_file.close()
     return
+
+
+def get_output(file_contents):
+    indicies = []
+    for i in range(len(file_contents)):
+        if file_contents[i] ==  "```\n":
+            indicies.append(i)
+    
+    output_list = []
+    # print(file_contents)
+    i = 0
+    j = 1
+    while True:
+        output_list.append(file_contents[indicies[i]+1:indicies[j]])
+        i += 2
+        j += 2       
+        if i >= len(indicies):
+            break
+
+    return output_list
+
+
 
 def parse_notes(file_contents):
     # two dictionaries????
 
     date_list = []
-    comment_list = []
     command_list = []
-    output_list = []
+    comment_list = []
+    output_list = get_output(file_contents)
 
     for entry in file_contents:
         if '####' in entry:
-            date_list.append(entry)
+            date_list.append(entry.strip('#### '))
         elif '- ' in entry:
-            # if there is no comment
-            comment_list.append(entry)
-        elif '```' in entry:
-            command_list.append(entry)
-        elif '`' in entry:
-            output_list.append(entry)
+
+            comment_list.append(entry.strip('- '))
+        elif '` ' in entry:
+            command_list.append(entry[:-1].strip('` '))
+
+    format_output(date_list, comment_list, command_list, output_list)
 
     return 
 
+def format_output(date,cmnt,cmd,out):
+    for i in range(len(date)):
+        print('\n')
+        print(date[i].strip('\n'))
+        print("Comment: " + cmnt[i])
+        print("Command: " + cmd[i])
+        for entry in out[i]:
+            print("### " + entry.strip('\n'))
+        
+
+    return
 
 
 if __name__ == "__main__":
