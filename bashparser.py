fc1557fe6fd6acc21e1c0cfefc7e3cdece1e97b5
Alex Rivie <rivie.alex@gmail.com>
2019-09-21 20:23:45 -0400

    parse by string added

--- bashparser.py
+++ bashparser.py
@@ -1,63 +1,23 @@
 #!/usr/bin/env python3
 
-import os
-import re
+import sys
+
 def main():
 
-    open_file = open("masterfile", 'r')
+    file = open(sys.argv[1], 'r')
+    dump = file.read().split('\n')
 
-    note_file_str = open_file.read()
-    note_file_list = change_color(note_file_str)
+    cmdlist = []
+    for line in dump:
+        if '$' in line:
+            cmdlist.append([line])
+        else: cmdlist[-1].append(line)
 
-    parsed_notes = parse_note_file(note_file_list)
-    find_cmd(parsed_notes)
-
-    return
-
-
-def change_color(note_file_str):
-    new_colored_string = ''
-
-    colour = re.findall("\[01;..m", note_file_str)
     
-    index = 0
-    leng_of_str = len(note_file_str)
-
-    while colour[0] in note_file_str:
-        new_colored_string = note_file_str.replace(colour[0], '[01;35m')
-        index += 1
-        if index == leng_of_str:
-            break
-
-    return new_colored_string.split('\n')
-
-
-def find_cmd(parsed_notes):
-    user_cmd = input("Enter command to scan for 'Enter' to get everything: ")
-
-    for line in parsed_notes:
-        for entry in line: 
-            if user_cmd in entry:
-                for index in range(len(line)):
-                    print(line[index].strip('\n'))
-
-    return
-
-def parse_note_file(note_file):
-    command_list = []
-    output_list = []
-
-
-    for line in note_file:
-        if 'bash -i' in line:
-            continue
-        elif '$' in line or '':
-            command_list.append([line])
-        else:
-            command_list[-1].append(line)
-
-    return command_list
-
+    for cmd in cmdlist:
+        if sys.argv[2] in cmd[0]:
+            for output in cmd:
+                print(output)
 
 if __name__ == "__main__":
     main()
